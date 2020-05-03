d51685041be555d450d040d256f5c57209445ed3
Exilesprx <campbell.andrew86@yahoo.com>
2020-05-03 01:36:36 -0400

    Smallest multiple

---
+++ src/euler/problem_five.clj
@@ -0,0 +1,8 @@
+(ns euler.problem-five
+  [:gen-class])
+
+(defn find-smallest-number
+  "Find the smallest number evenly divisible by all numbers up to x"
+  [x]
+  1
+  )
---
+++ test/euler/problem_five_test.clj
@@ -0,0 +1,10 @@
+(ns euler.problem-five-test
+  (:require [clojure.test :refer :all]
+            [euler.problem-five :refer :all]))
+
+(deftest problem-five
+
+  (testing "The smallest number divisible by 1-10 is 2520"
+    (is (= 2520 (find-smallest-number 10)))
+    )
+)
