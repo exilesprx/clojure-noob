8d9e9de5bfd77761db462860132d8cda8786691a
Exilesprx <campbell.andrew86@yahoo.com>
2020-05-06 22:45:23 -0400

    Smallest multiple
    Find the smallest positive number that is evenly divisible by all of the 
    numbers from 1 to n.

--- src/euler/problem_five.clj
+++ src/euler/problem_five.clj
@@ -1,8 +1,21 @@
 (ns euler.problem-five
   [:gen-class])
 
-(defn find-smallest-number
-  "Find the smallest number evenly divisible by all numbers up to x"
-  [x]
-  1
-  )
+  (defn find-smallest-number
+    "Find the smallest number evenly divisible by all numbers up to n"
+    [n]
+    (loop [y 1]
+      ; Loop all numbers up to n and  get the remainder
+      (let [res (for [x (range 1 n)
+                      :let [div (mod y x)]]
+                      div)]
+
+        ; If the sum is zero, the smallest number was found
+        ; so return it. Otherwise, increment y and restart.
+        (if (= (reduce + res) 0)
+          y
+          (recur (inc y))
+          )
+        )
+      )
+    )
