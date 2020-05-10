9e78818da905db6d3d01f762501811605db5adf6
Exilesprx <campbell.andrew86@yahoo.com>
2020-05-09 20:01:20 -0400

    Sum square of difference
    Find the difference between the sum of the squares of the first one 
    hundred natural numbers and the square of the sum.

--- src/euler/problem_6/main.clj
+++ src/euler/problem_6/main.clj
@@ -1,8 +1,26 @@
 (ns euler.problem-6.main
   [:gen-class])
 
+(defn sum-square-of
+  "Finds the sum of the squares of the first n natural numbers."
+  [n]
+  (reduce
+    +
+    (for [x (range (inc n))]
+      (int (Math/pow x 2))
+      )
+    )
+  )
+
+(defn square-sum-of
+  "Finds the square of the sum of the first n natural numbers."
+  [n]
+  (int (Math/pow (reduce + (range 1 (inc n))) 2)
+    )
+  )
+
 (defn find-sum-square-difference
   "Finds the difference between the sum of the squares of the first n natural numbers and the square of the sum."
-  []
-  1
+  [n]
+  (- (square-sum-of n) (sum-square-of n))
   )
