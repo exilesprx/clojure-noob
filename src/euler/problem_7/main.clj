b14357db1ff4881f79d38990bab4a0e610eb7f0f
Exilesprx <campbell.andrew86@yahoo.com>
2020-06-09 20:45:10 -0400

    Initial test and function setup

---
+++ src/euler/problem_7/main.clj
@@ -0,0 +1,7 @@
+(ns euler.problem-7.main
+  (:gen-class))
+
+(defn find-nth-prime
+  "Finds the nth prime number"
+  [n]
+  1)
---
+++ test/euler/problem_7/test.clj
@@ -0,0 +1,8 @@
+(ns euler.problem-7.test
+  (:require [clojure.test :refer :all]
+            [euler.problem-7.main :refer :all]))
+
+(deftest test-problem-seven
+
+  (testing "13 is the 6th prime number"
+    (is (= (find-nth-prime 6) 13))))
